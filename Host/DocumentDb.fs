d5a7660eeb8b656da700ed2668d7877a77af265b
Alexander Prooks <aprooks@live.ru>
2017-01-20 01:21:58 +0200

    feat: persisted actors
    Signed-off-by: Alexander Prooks <aprooks@live.ru>

--- Host/DocumentDb.fs
+++ Host/DocumentDb.fs
@@ -1,5 +1,9 @@
 module DocumentDb
 
+open System.Net
+
+open Orleankka.FSharp
+
 open Microsoft.Azure.Documents
 open Microsoft.Azure.Documents.Client
 
@@ -9,14 +13,26 @@ let createDb (dbName:string) (client:DocumentClient) =
     let db = new Database()
     db.Id <- dbName.ToLower()
     client.CreateDatabaseIfNotExistsAsync(db,new RequestOptions())
-    |> Async.AwaitTask
 
 let createCollection (collectionName:string) (db: Database) (client:DocumentClient)= 
     let collection = new DocumentCollection()
     collection.Id<-collectionName.ToLower()
     client.CreateDocumentCollectionIfNotExistsAsync(db.CollectionsLink,collection,new RequestOptions())
-    |> Async.AwaitTask
 
 let upsert (doc:obj) (collection:DocumentCollection) (client:DocumentClient) = 
     client.UpsertDocumentAsync(collection.DocumentsLink, doc)
-    |> Async.AwaitTask
+
+let loadDocument<'a> id (collection:DocumentCollection) (db:Database) (client:DocumentClient) = 
+      task{
+        try
+            let! res = UriFactory.CreateDocumentUri(db.Id,collection.Id,id)
+                    |> client.ReadDocumentAsync
+            return res |> Some        
+        with 
+        | :? DocumentClientException as ex ->
+                match ex.StatusCode.Value with 
+                |  HttpStatusCode.NotFound -> return None
+                | _ -> 
+                    raise ex
+                    return None
+      }
--- Host/Host.fs
+++ Host/Host.fs
@@ -7,6 +7,8 @@ open FSharp.Data
 
 open DocumentDb
 
+open Newtonsoft.Json
+
 type Id = string
 
 type TestDoc ={
@@ -19,6 +21,7 @@ open Orleankka.FSharp
 open Orleankka.FSharp.Configuration
 open Orleankka.FSharp.Runtime
 open System.Reflection
+
 
 
 module Org = 
@@ -35,19 +38,77 @@ module Org =
         createdAt: System.DateTime
     }
     
+    let loadState id = task {
+            let! db = createDb "actors" DocumentDb.client
+            let! coll = createCollection "states" db.Resource DocumentDb.client
+            let! state = loadDocument id coll.Resource db.Resource DocumentDb.client
+            return match state with
+                   | Some doc -> 
+                                 JsonConvert.DeserializeObject<State>(doc.Resource.ToString())
+                                 |> Some
+                   | None -> None 
+    }
+
+    let uploadState state = task{
+            let! db = createDb "actors" DocumentDb.client
+            let! coll = createCollection "states" db.Resource DocumentDb.client
+            return! upsert state coll.Resource DocumentDb.client
+    }
+
     type Organization()=
         inherit Actor<Contract>()
-        override this.Receive msg = task{
-            match msg with 
-            | Create (name,adminEmail) -> return nothing
+        let mutable s = {id = ""; name=""; adminEmail  = "";createdAt = DateTime.Now}
+
+        override this.Activate () = 
+            printfn "activated with base.id %s" base.Id
+            let id = base.Id
+            task {
+                printfn "activated with %s" id
+                let! doc = loadState id
+                match doc with
+                | Some state ->
+                    s <- state
+                | None -> ignore()            
+            }
+
+        override this.Receive msg = 
+            printfn "recieved msg of %s" (msg.GetType().ToString())
+            let id = base.Id
+            task {
+                match msg with 
+                | Create (name,adminEmail) -> 
+                    if s.id <> "" then
+                        failwith "cannot create twice"
+
+                    let! result = {
+                                            id = id
+                                            name = name
+                                            adminEmail = adminEmail
+                                            createdAt = System.DateTime.Now
+                                    }
+                                    |> uploadState 
+                    return nothing 
         }
 
-let TestDocumentDb ()= async {
+let TestDocumentDb ()= task {
     let! dbResponse = createDb "test" DocumentDb.client
     
     let! collection = createCollection "testcollection" dbResponse.Resource DocumentDb.client
 
-    return! upsert {id="Hello1"; name="World"} collection.Resource DocumentDb.client
+    let! upsertResult = upsert {id="Hello1"; name="World"} collection.Resource DocumentDb.client
+    printfn "upserted doc %A" upsertResult.Resource.Id   
+
+    let! existingDoc = loadDocument "Hello1" collection.Resource dbResponse.Resource DocumentDb.client
+    match existingDoc with 
+    | Some d -> printfn "existingDoc loaded %A" d.Resource
+    | None -> printfn "failed to load doc"
+    
+    let! nonExistingDoc = loadDocument "Hello2" collection.Resource dbResponse.Resource DocumentDb.client
+    match nonExistingDoc with 
+    | Some d -> printfn "existingDoc loaded %A" d.Resource
+    | None -> printfn "failed to load doc"
+
+    return nothing
 }
 
 let ScaffoldGrains (system:IActorSystem) ()= task{
@@ -57,13 +118,13 @@ let ScaffoldGrains (system:IActorSystem) ()= task{
 }
 open System.Threading.Tasks
 
-
 [<EntryPoint>]
 let main argv =
     printfn "running doc db tests"
-    let res = TestDocumentDb() |> Async.RunSynchronously 
-    res.Resource.Id    
-    |> printfn "finished uploading: %A"
+    let res = TestDocumentDb |> Task.run 
+    match res with
+    | Choice1Of2 d -> printfn "finished documentdb upload"
+    | Choice2Of2 exn -> printfn "exception running docDb upload\n %s" exn.Message
 
     printfn "starting actor system"
     
@@ -73,11 +134,12 @@ let main argv =
     
     printfn "actor system started"
 
-    system
-    |> ScaffoldGrains
-    |> Task.run 
-    |> ignore
-
-    printfn "scaffolding complete"
+    let res = 
+        system
+        |> ScaffoldGrains
+        |> Task.run 
+    match res with 
+    | Choice1Of2 _ -> printfn "scaffolding complete"
+    | Choice2Of2 exn -> printfn "exeption on scaffolding:\n %s\n %s" exn.Message exn.StackTrace
 
     0 // return an integer exit code
