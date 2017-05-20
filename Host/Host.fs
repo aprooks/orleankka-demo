55ca8012feb7e6868e39890315229cf7b6e70693
Alexander Prooks <aprooks@live.ru>
2017-05-20 11:51:47 +0200

    demo starter
    Signed-off-by: Alexander Prooks <aprooks@live.ru>

--- Host/Host.fs
+++ Host/Host.fs
@@ -10,60 +10,23 @@ open Orleankka.FSharp
 open Orleankka.FSharp.Configuration
 open Orleankka.FSharp.Runtime
 
-type Id = string
 
-type TestDoc ={
-    id: Id
-    name: string
-}
-
-open DocumentDb
-open Microsoft.Azure.Documents.Client
-let TestDocumentDb ()= task {
-    let! dbResponse = createDb "test" DocumentDb.client
+module Pingers =
+    type Message = 
+    | Ping
     
-    let! collection = createCollection "testcollection" dbResponse.Resource DocumentDb.client
+    type Pinger() = 
+        inherit Actor<Message>()
 
-    let! upsertResult = upsert {id="Hello1"; name="World"} collection.Resource DocumentDb.client
-    printfn "upserted doc %A" upsertResult.Resource.Id   
+        override this.Receive msg = task{
+            match msg with
+            | Ping -> return response("Pong")
+        }
 
-    let printLoadResult (res:ResourceResponse<Microsoft.Azure.Documents.Document> option) =
-        match res with
-        | Some d -> printfn "existingDoc loaded %A" d.Resource
-        | None -> printfn "failed to load doc"
-
-    let! existingDoc = loadDocument "Hello1" collection.Resource dbResponse.Resource DocumentDb.client
-    existingDoc |> printLoadResult
-
-    let! nonExistingDoc = loadDocument "Hello2" collection.Resource dbResponse.Resource DocumentDb.client
-    nonExistingDoc |> printLoadResult
-
-    return nothing
-}
-
-let ScaffoldGrains (system:IActorSystem) ()= task{
-   let actor = ActorSystem.actorOf<Org.Organization>(system, "test_id")
-   printfn "loading state"
-   let! state = actor.Ask<Org.State> Org.GetState
-
-   printfn "loaded state with id: %s" state.name
-
-   try
-       do! actor <! Org.Create("test_id", "test","admin@example.com")
-   with
-   | ex -> printfn "creation failed with ex: %s" ex.Message
-}
-
-open System.Threading.Tasks
+open Pingers
 
 [<EntryPoint>]
 let main argv =
-    printfn "running doc db tests"
-    let res = TestDocumentDb |> Task.run 
-    match res with
-    | Choice1Of2 d -> printfn "finished documentdb upload"
-    | Choice2Of2 exn -> printfn "exception running docDb upload\n %s" exn.Message
-
     printfn "starting actor system"
     
     let system = [|System.Reflection.Assembly.GetExecutingAssembly()|]
@@ -72,14 +35,15 @@ let main argv =
     
     printfn "actor system started"
 
-    let res = 
-        system
-        |> ScaffoldGrains
-        |> Task.run 
-    match res with 
-    | Choice1Of2 _ -> printfn "scaffolding complete"
-    | Choice2Of2 exn -> printfn "exeption on scaffolding:\n %s\n %s \n %s" exn.Message exn.StackTrace (exn.InnerException.ToString())
-    
+ 
+    fun _ -> task {
+        let pinger =  ActorSystem.actorOf<Pinger>(system,"myId")
+        let! res = pinger <? Ping
+        printfn "received: %s" res //Pong
+    } 
+    |> Task.run 
+    |> ignore
+
     Console.ReadLine() |> ignore
     
     0 // return an integer exit code
